897cef5e813b0146d3e71ee9a4b41c23e07d797e
astoninfer <astoninfer@gmail.com>
2024-06-18 21:56:10 +0930

    init

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:8080',
+    // baseURL: 'http://localhost:8080',
+    baseURL: 'http://140.238.201.7:8080',
     headers: {
         'Content-type': 'application/json',
     },
