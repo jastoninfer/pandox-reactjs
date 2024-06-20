bedb90962a6586dfeac3179ad0c9d9941d50e212
astoninfer <astoninfer@gmail.com>
2024-06-20 18:06:14 +0930

    init

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     // baseURL: 'http://localhost:8080',
-    baseURL: 'http://140.238.201.7:8080',
+    baseURL: 'https://www.pandox.xyz',
     headers: {
         'Content-type': 'application/json',
     },
